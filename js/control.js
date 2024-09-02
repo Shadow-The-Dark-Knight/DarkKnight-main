efcf9e44d5c35732a4fe300128842597f2b6e2d9
Shadow <nintendothing123@gmail.com>
2024-09-02 01:04:36 -0700

    allows toggling button to work until chat

---
+++ js/control.js
@@ -0,0 +1,33 @@
+
+
+
+  const exitBtn = document.querySelector('.js-exit');
+  const navBar = document.querySelector('.js-navbar');
+  const returnSetBtn = document.querySelector('.js-icon');
+
+  function escape(selector) {
+    if(selector === 'open') {
+      exitBtn.style.display = "";
+      navBar.style.display = "";
+      returnSetBtn.style.display = "none";
+      return;
+    } 
+    if(selector === 'closed') { 
+      exitBtn.style.display = "none";
+      navBar.style.display = "none";
+      returnSetBtn.style.display = "";
+
+      return;
+    }
+    }
+
+
+   returnSetBtn.onclick = () => {
+    escape('open');
+   }
+    
+   exitBtn.onclick = () => {
+    escape('closed');
+   }
+
+    escape('open');
