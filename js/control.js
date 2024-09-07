28cec6ea98207b7f91e14558c8a19c1853c71791
Amir Shanti <amirvs9675@gmail.com>
2024-09-06 18:43:07 -0700

    fixed quality and controls format but controls section still has problems

--- js/control.js
+++ js/control.js
@@ -1,2 +1,31 @@
 import { settings } from "./quality.js";
 settings();
+
+
+window.onload = () => {
+const mouseBtn = document.querySelector('.js-button');
+
+function mouse(selector) {
+    if (selector === 'clicked') {
+        mouseBtn.classList.add('js-button');
+        return;
+    }
+    if(selector === 'notclicked') {
+        mouseBtn.classList.remove('js-button');
+        return
+    }
+}
+
+
+
+mouseBtn.onclick = () => {
+    mouse('clicked')
+};
+
+mouseBtn.onclick = () => {
+    mouse('notclicked')
+};
+
+
+mouse('notclicked');
+};
